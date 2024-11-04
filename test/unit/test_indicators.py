95489c0a0f1ede9d15eae91369c375a340b880cb
teihenn <hm.hr.yossy@gmail.com>
2024-11-04 17:39:12 +0900

    test: Add test_indicators.py

---
+++ test/unit/test_indicators.py
@@ -0,0 +1,35 @@
+import unittest
+
+import pandas as pd
+
+from src.indicators import calculate_rci
+
+
+class TestIndicators(unittest.TestCase):
+    def test_calculate_rci_100(self):
+        """
+        RCI計算のテスト(RCI=100)
+        テストデータ参考: https://kabu.com/investment/guide/technical/14.html
+        """
+        test_data = pd.DataFrame({"Close": [500, 510, 515, 520, 530]})
+
+        # 期間を変えてテスト
+        period = 5
+        expected = 100.0
+
+        actual = calculate_rci(test_data, period)
+        self.assertEqual(actual, expected)
+
+    def test_calculate_rci_minus100(self):
+        """
+        RCI計算のテスト(RCI=-100)
+        テストデータ参考: https://kabu.com/investment/guide/technical/14.html
+        """
+        test_data = pd.DataFrame({"Close": [530, 520, 515, 510, 500]})
+
+        # 期間を変えてテスト
+        period = 5
+        expected = -100.0
+
+        actual = calculate_rci(test_data, period)
+        self.assertEqual(actual, expected)
