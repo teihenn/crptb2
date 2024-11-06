e079f54fa2f5135cf55429ae06cc54b929bdeaef
teihenn <hm.hr.yossy@gmail.com>
2024-11-07 00:19:32 +0900

    feat: Add base_strategy.py

---
+++ src/strategy/base_strategy.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+from src.config.config import Config
+from src.utils.discord import DiscordNotifier
+from src.utils.logger import Logger
+
+
+class BaseStrategy(ABC):
+    """
+    ストラテジーの基底クラス
+    """
+
+    def __init__(self, config: Config):
+        """
+        Parameters:
+        -----------
+        config : Config
+            設定オブジェクト
+        """
+        self.position = None  # 'long' or 'short' or None
+        self.logger = Logger.get_logger()
+
+        # Discord通知の設定
+        self.discord = DiscordNotifier(config.discord.webhook_url)
+
+    @abstractmethod
+    def calculate_indicators(self, df: pd.DataFrame) -> pd.DataFrame:
+        """必要なインジケーターを計算"""
+        pass
+
+    @abstractmethod
+    def should_entry(self, df: pd.DataFrame) -> tuple[bool, str]:
+        """エントリー判断。(エントリーすべきか, ポジション方向)を返す"""
+        pass
+
+    @abstractmethod
+    def should_exit(self, df: pd.DataFrame) -> bool:
+        """決済判断"""
+        pass
